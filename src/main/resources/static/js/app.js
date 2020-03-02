d17a5c55de3f19542e7c2722f6b1446ba54753b2
devraj <devraj@172.22.50.105>
2020-03-02 16:59:00 +0400

    reg

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -8,7 +8,21 @@ app.controller('Login', ['$scope', function($scope) {
 	$scope.form = {}
 	
 	$scope.authenticate = function() {
-		alert("login success," + this.form.username + " " + this.form.password);
+		alert("login success," + JSON.stringify(this.form));
+		$scope.form = {}
+	}
+}]);
+
+app.controller('Registration', ['$scope','$http', function($scope, $http) {
+	$scope.form = {}
+	
+	$scope.register = function() {
+		this.$http.post('/register', JSON.stringify(this.form))
+			.success(function (data, status, headers, config) {
+				$location.path("/login?regSuccess=true");
+			}).error(function (data, status, headers, config) {
+				$location.path("/register?error=true");
+			});
 		$scope.form = {}
 	}
 }]);
