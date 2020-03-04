af5107876bc25464ea799c232d61bbb4e1fc402c
PRADEEP <PRADEEP@172.22.61.237>
2020-03-04 23:33:51 +0400

    Search page

--- src/main/resources/static/js/adminApp.js
+++ src/main/resources/static/js/adminApp.js
@@ -71,3 +71,55 @@ app.controller('createChannelController', function($scope, $http, $window) {
 	}
 	
 });
+
+
+app.controller('searchChannelController', function($scope, $http, $window) {
+	$scope.form = {}
+	$scope.numReg=/^[0-9]+$/;
+	$scope.idMinLn=5;
+	$scope.idMaxLn=20;
+	$scope.search=0;
+	
+	$http.get('/user', {
+	    headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}})
+		.success(function (data, status, headers, config) {
+			if (data.roles == 'ROLE_USER') {
+				$window.location.href = "/userHome";
+			}
+		});
+	
+	$scope.searchChannel = function() {
+		$scope.error=false;
+		$scope.success=false;
+		$http.post('/admin/channels/', JSON.stringify(this.form),
+				{headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}})
+			.success(function (data, status, headers, config) {
+				$scope.success=true;
+				$('.alert-info').text('Channel successfully created with ID:' + data.id);
+				$window.scrollTo(0, 0);
+				$scope.createChannelForm.$setUntouched();
+				$scope.form = {}
+			}).error(function (data, status, headers, config) {
+				$scope.error=true;
+				$('.alert-danger').text(data.message);
+				$window.scrollTo(0, 0);
+			});
+	}
+	
+	$scope.handleError = function($error, patternMessage) {
+		this.handleError($error, patternMessage, null, null);
+	}
+	
+	$scope.handleError = function($error, patternMessage, minLength, maxLength) {
+		if ($error.required) {
+			return " (Required)";
+		} else if ($error.pattern) {
+			return patternMessage;
+		} else if (minLength && $error.minlength) {
+			return " Minimum Length:" + minLength;
+		} else if (maxLength && $error.maxlength) {
+			return " Maximum Length:" + maxLength;
+		}
+	}
+	
+});
