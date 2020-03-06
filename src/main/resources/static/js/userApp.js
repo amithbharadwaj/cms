55e1f3e33c309a4e687daf5f7e796491ad755658
PRADEEP <PRADEEP@192.168.1.107>
2020-03-06 21:30:59 +0400

    User channels

--- src/main/resources/static/js/userApp.js
+++ src/main/resources/static/js/userApp.js
@@ -2,12 +2,14 @@ var app = angular.module('userApp',[]);
 
 app.controller('userChannels', function($scope, $http, $window) {
 	$scope.isLoggedIn = false;
-	$scope.channels='';
+	$scope.channels=null;
 	
-	$http.get('/user/channels', {
+	$http.get('/user/channels?pageNo=0&pageSize=999', {
 	    headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}})
 		.success(function (data, status, headers, config) {
-			$scope.channels=data;
+			if (data.length > 0) {
+				$scope.channels=data;
+			}
 		});
 	
 });
